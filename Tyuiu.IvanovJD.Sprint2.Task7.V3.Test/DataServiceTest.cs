7add3ee371ecab4b363bc79c55e50a59d61eff57
Tyuiu.IvanovJD@outlook.com <yarikiro@mail.ru>
2023-10-18 18:00:54 +0300

    Task7

---
+++ Tyuiu.IvanovJD.Sprint2.Task7.V3.Test/DataServiceTest.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using Tyuiu.IvanovJD.Sprint2.Task7.V3.Lib;
+
+namespace Tyuiu.IvanovJD.Sprint2.Task7.V3.Test
+{
+    [TestClass]
+    public class DataServiseTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            DataService ds = new DataService();
+            double x = 0.5;
+            double y = 0.5;
+            bool res = ds.CheckDotInShadedArea(x, y);
+            bool wait = true;
+            Assert.AreEqual(res, wait);
+        }
+    }
+}
---
+++ Tyuiu.IvanovJD.Sprint2.Task7.V3/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Tyuiu.IvanovJD.Sprint2.Task7.V3.Lib;
+using System.Threading.Tasks;
+
+namespace Tyuiu.IvanovJD.Sprint2.Task7.V3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Title = "Спринт #1 | Выполнил: Иванов Я. Д. | ПКТБ-23-2";
+            Console.WriteLine("****************************************************************************");
+            Console.WriteLine("* Спринт #2                                                                *");
+            Console.WriteLine("* Тема: Базовые навыки работы в С#                                         *");
+            Console.WriteLine("* Задание #7                                                               *");
+            Console.WriteLine("* Вариант #3                                                                *");
+            Console.WriteLine("* Выполнил: Иванов Ярослав Дмитриевич | ПКТБ-23-2                          *");
+            Console.WriteLine("****************************************************************************");
+            Console.WriteLine("* УСЛОВИЕ:                                                                 *");
+            Console.WriteLine("* Написать программу, которая запрашивает у пользователя исходные данные,  *");
+            Console.WriteLine("* выполняет указанные расчёты и печатает результат на экране.              *");
+            Console.WriteLine("****************************************************************************");
+            Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                         *");
+            Console.WriteLine("****************************************************************************");
+
+            Console.WriteLine("Введите значение x: ");
+            double x = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Введите значение y: ");
+            double y = Convert.ToDouble(Console.ReadLine());
+
+            DataService ds = new DataService();
+            bool res = ds.CheckDotInShadedArea(x, y);
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* РЕЗУЛЬТАТ                                                               *");
+            Console.WriteLine("***************************************************************************");
+
+            Console.WriteLine(res);
+
+            Console.ReadKey();
+        }
+    }
+}
+        
+    
+
